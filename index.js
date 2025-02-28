23cb52d32bde7bd293dac5abd17abd1aa459bd05
JesusBlan <jezublanco@gmail.com>
2025-02-28 18:48:45 +0100

    adde a hash function to generate a hash code with the key and reduce it to the range of the hash map size

--- index.js
+++ index.js
@@ -3,4 +3,25 @@ class hashMap{
         this.size = size;
         this.loadFactor = loadFactor;
     }
+
+    hash(key){
+        
+        let hashCode = 0;
+               
+        for (let i = 0; i < key.length; i++) {
+            hashCode = 31 * hashCode + key.charCodeAt(i);
+        }
+         
+        return hashCode % this.size;
+    }
+
+
 }
+
+
+
+let test = new hashMap();
+
+
+
+console.log(test.hash("231412235"));
