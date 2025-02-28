5c1ddf03b9fce53b49a72ff3248d6999559f072c
JesusBlan <jezublanco@gmail.com>
2025-02-28 19:02:06 +0100

    added a function to put items in the hash map

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ class hashMap{
     constructor(size = 16, loadFactor = 0.8){
         this.size = size;
         this.loadFactor = loadFactor;
+        this.data = [];
     }
 
+    //take the key and return a hash code to index the new value
     hash(key){
-        
         let hashCode = 0;
                
         for (let i = 0; i < key.length; i++) {
@@ -15,13 +16,23 @@ class hashMap{
         return hashCode % this.size;
     }
 
+    //put an item in the hashed index
+    setItem(key, value){
+        let index = this.hash(key);
+
+        if (!this.data[index]){
+            this.data[index] = [];
+        }
+        this.data[index].push([key, value])
+    }
+
 
 }
 
 
 
 let test = new hashMap();
-
-
+test.setItem("pedro", "picapiedra")
+console.log(test.data)
 
 console.log(test.hash("231412235"));
